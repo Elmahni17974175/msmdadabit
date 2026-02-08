1aabe19af5929b67cac300a83e351b57e0e8f6a1
Elmahni17974175 <124938207+Elmahni17974175@users.noreply.github.com>
2026-02-08 19:58:26 +0100

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,5 @@
 {
-  "name": "msmdadabit",
+  "name": "msmdadabit", 
   "version": "0.0.5",
   "description": "MSM AI Handler + Smart Transport (DaDa:bit + WonderCam via dadabit)",
   "license": "MIT",
